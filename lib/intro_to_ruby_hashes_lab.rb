18f819a3601eebae049532f64e75ca679828628c
Travis Prol <travisjohnprol@gmail.com>
2020-05-07 22:03:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,22 @@
 def new_hash
-  # return an empty hash
+  new_hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {
+    city: "Sparta"
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer_hash = {
+    name: "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  number_hash = {
+    id: number
+  }
 end
